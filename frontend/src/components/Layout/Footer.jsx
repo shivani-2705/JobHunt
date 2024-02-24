737206dd120fc4063809dc6fa892fba54128a6be
shivani-2705 <131511599+shivani-2705@users.noreply.github.com>
2024-02-24 10:13:37 +0530

    Add files via upload

---
+++ frontend/src/components/Layout/Footer.jsx
@@ -0,0 +1,26 @@
+import { useContext } from 'react';
+import {Context} from '../../main';
+import {Link } from 'react-router-dom';
+import {FaFacebook, FaYoutube, FaLinkedin} from "react-icons/fa"
+import {RiInstagramFill} from "react-icons/ri"
+
+const Footer = () => {
+  const {isAuthorized} = useContext(Context);
+
+  return (
+    <footer className={isAuthorized ? "footerShow" : "footerHide"}>
+      <div>&copy; All Rights Reserved By JobHunt.</div>
+      <div>
+         {/* add links for social media */}
+        <Link to={"/"} target='_blank'> <FaFacebook/> </Link>
+        <Link to={"/"} target='_blank'> <FaLinkedin/> </Link>
+        <Link to={"/"} target='_blank'> <FaYoutube/> </Link>
+        <Link to={"/"} target='_blank'>  <RiInstagramFill/> </Link>
+      </div>
+
+
+    </footer>
+  )
+}
+
+export default Footer
